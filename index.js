f847938cf13a2bff5165dd108502c00fcbf3c0c7
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-05-28 19:22:35 -0400

    commit tree e85f2c6de570ad9625c6c686d2ed0dcc9cccc253

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+//newHeader.getElementById('victory')
+
+const header = document.getElementById('victory')
+header.innerHTML = "Alex is the champion"
+
+
